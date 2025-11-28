81fd2b31ae8b0e891ad1634356e8f927e8eb59a1
Lalak <lalak@webkorps.com>
2025-11-28 12:42:20 +0530

    First react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const toggleMode = (mode) => {
+    if (mode === 'dark') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success");
+    } else if (mode === 'red') {
+      setMode('red');
+      document.body.style.backgroundColor = '#bc5454ff';
+      showAlert("Red mode has been enabled", "success");
+    } else if (mode === 'green') {
+      setMode('green');
+      document.body.style.backgroundColor = '#4bb543ff';
+      showAlert("Green mode has been enabled", "success");
+    } else if (mode === 'blue') {
+      setMode('blue');
+      document.body.style.backgroundColor = '#3b5998ff';
+      showAlert("Blue mode has been enabled", "success");
+    }else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+      <TextForm heading="Enter a text to analyze below" mode={mode} showAlert={showAlert} />
+      {/* <About/> */}
     </div>
+    </>
   );
 }
 
